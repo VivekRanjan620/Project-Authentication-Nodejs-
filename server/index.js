011b9953546eecf71d7ae1f5e183e6969f2db6f7
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-03-21 23:57:22 +0530

    Authentication

---
+++ server/index.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let mongoose = require('mongoose')
+let userRouter = require('../route/routeUser')
+mangoose.conncet(' mongodb://127.0.0.1:27017/fsd')
+.then(()=>{
+   console.log('db');
+}).catch((err)=>{
+ console.log(err);
+});
+
+
+
+let app = express();
+app.use(express.json())
+app.use('/api',userRouter)
+// app.get('/',(req,res)=>{
+//     res.send('hello')
+// })
+app.listen(4000,()=>{
+    console.log('server start on port no 4000');
+})
---
+++ server/route/routeLogin.js
@@ -0,0 +1,11 @@
+// let express = require("express");
+// let router = express.Router();
+// let user = require("../model/userdb");
+// let bcrypt = require("bcrypt");
+
+// router.post("/signup", async (req, res) => {
+ 
+    
+// });
+
+// module.exports=router
